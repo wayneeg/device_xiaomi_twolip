e9b142f84f32335a6a3db7723e75d5bf461081dc
Edwin Moquete <edwinmmoquete@gmail.com>
2021-09-17 18:23:59 -0400

    twolip: Build QTI thermal HAL
    Signed-off-by: muti605 <mustafa.sahin605@yandex.com>

--- device.mk
+++ device.mk
@@ -383,6 +383,9 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml
 	
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 PRODUCT_COPY_FILES += \
 	 $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
