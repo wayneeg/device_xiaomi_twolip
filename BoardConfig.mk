b899b2117090592cad14323c493a738f3a6573db
muti605 <mustafa.sahin605@yandex.com>
2021-10-09 19:41:10 +0300

    twolip: Disable QCOM sepolicy too
    Signed-off-by: muti605 <mustafa.sahin605@yandex.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,7 +148,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+#include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 #BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 #BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
