3e969b355e44b94190a56e8530c10e8609da4732
TogoFire <italomellopereira@gmail.com>
2021-10-10 15:37:23 +0300

    twolip: Build keymaster 4.0 and add it to hidl entry
    Signed-off-by: muti605 <mustafa.sahin605@yandex.com>

--- device.mk
+++ device.mk
@@ -261,6 +261,7 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0.vendor \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@4.0-impl \
     android.hardware.keymaster@4.0.vendor \
     android.hardware.keymaster@4.1.vendor \
     android.hardware.keymaster@4.0-service \
